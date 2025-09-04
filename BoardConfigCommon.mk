fdac6f418c68134a8759eb36d108923c1ca40d32
Yumi Yukimura <me.cafebabe@gmail.com>
2025-09-04 23:17:45 +0800

    msm8953-common: Prepend soong ns to lib_driver_cmd_qcwcn
    Change-Id: I09bfb412e3554d349c4ad123c83d46a7933611d5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -129,10 +129,10 @@ BOARD_AVB_ENABLE := false
 # Wi-Fi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
+BOARD_HOSTAPD_PRIVATE_LIB := //hardware/qcom-caf/wlan/qcwcn:lib_driver_cmd_qcwcn
 BOARD_WLAN_DEVICE := qcwcn
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := //hardware/qcom-caf/wlan/qcwcn:lib_driver_cmd_qcwcn
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_AVOID_IFACE_RESET_MAC_CHANGE := true
