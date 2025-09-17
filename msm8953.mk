c2b43f62c33087a629867ffa3a6904a509ed71d4
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-09-17 14:56:40 +0800

    msm8953-common: drop qti powerhal
    since dt2w is not going to be toggle-able on settings anymore, lets just remove it.
    
    to disable echo 0 > /proc/touchpanel/wakeup_gesture

--- msm8953.mk
+++ msm8953.mk
@@ -217,10 +217,6 @@ PRODUCT_PACKAGES += \
     vendor_dsp_mountpoint \
     vendor_firmware_mnt_mountpoint
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
