fa81b3d9bd53f807365c577b406d80fea48b88e8
Yumi Yukimura <me.cafebabe@gmail.com>
2025-12-27 01:14:40 +0800

    msm8953-common: Migrate wcnss_service variables to soong
    Change-Id: I2191d331b6bf8897e32b4cb0031c2776333da8ad
    
    https://github.com/Mi-Thorium/android_device_xiaomi_mithorium-common/commit/6524dc161727e1d8514177aa186eecfa9e7c8baa

--- msm8953.mk
+++ msm8953.mk
@@ -325,6 +325,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+$(call soong_config_set_bool,wcnss_service,uses_qcom_wcnss_qmi,true)
+$(call soong_config_set,wcnss_service,wcnss_qmi_include_dir,$(LOCAL_PATH)/wifi/include)
+
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
     firmware_wcnss_qcom_cfg.bin_symlink \
