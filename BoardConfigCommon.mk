b03f70462d9e62d79dc76a2345ba9c0c2f2ac3e1
Yumi Yukimura <me.cafebabe@gmail.com>
2026-01-01 19:34:05 +0800

    fixup! msm8953-common: Prepend soong ns to lib_driver_cmd_qcwcn
    Change-Id: I39c50fd9b565abff42fd82193acb75bf4dd85ca6
    
    https://github.com/Mi-Thorium/android_device_xiaomi_mithorium-common/commit/480559bacef8f03ce4d8ee6f505c0548f54063e1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -126,10 +126,10 @@ BOARD_AVB_ENABLE := false
 # Wi-Fi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := //hardware/qcom-caf/wlan:lib_driver_cmd_qcwcn
+BOARD_HOSTAPD_PRIVATE_LIB := //hardware/qcom-caf/wlan/qcwcn:lib_driver_cmd_qcwcn
 BOARD_WLAN_DEVICE := qcwcn
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := //hardware/qcom-caf/wlan:lib_driver_cmd_qcwcn
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := //hardware/qcom-caf/wlan/qcwcn:lib_driver_cmd_qcwcn
 TARGET_USES_QCOM_WCNSS_QMI := true
 TARGET_WCNSS_QMI_INCLUDE_DIR := $(COMMON_PATH)/wifi/include
 PRODUCT_VENDOR_MOVE_ENABLED := true
